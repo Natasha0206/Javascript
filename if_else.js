700c11903a8b8f4b9c9ab169ab53db16cd529525
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-12-13 00:05:20 +0530

    Add files via upload

---
+++ if_else.js
@@ -0,0 +1,10 @@
+let firstname = "Natasha";
+let age = 20;
+let isMarried = false;
+
+if (isMarried == false) {
+    console.log(firstname + "is not married");
+}
+else {
+    console.log(firstname + "is married");
+}
