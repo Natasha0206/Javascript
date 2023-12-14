da17164800f49417c019bcc1d66d442675c813b8
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-12-14 10:25:38 +0530

    Add files via upload

--- CutIt.js
+++ CutIt.js
@@ -1,8 +1,15 @@
 function cutIt(str, startIndex, endIndex){
     let newStr = "";
     for (let i=0; i<str.length; i++){
-        if (i>= startIndex && i <=endIndex){
+        if (i>= startIndex && i < endIndex){
             newStr = newStr + str[i];
         }
     }
+    return newStr;
 }
+
+const value = "harkirat Singh";
+
+let ans2 = value.slice(2,5)
+console.log(ans2);
+console.log(cutIt(value, 2, 5));
