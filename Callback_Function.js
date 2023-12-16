65772f5f88bb17ec0c7f41d99bab9cf4c90f89b2
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-12-16 23:05:16 +0530

    Add files via upload

---
+++ Callback_Function.js
@@ -0,0 +1,17 @@
+function square(n){
+    return n * n;
+} 
+function cube(n){
+    return n * n * n;
+} 
+
+
+function SumOfSomething(a, b, callback){
+    const val1 = callback(a);
+    const val2 = callback(b);
+
+    return val1 + val2;
+}
+
+console.log(SumOfSomething(1,2, cube));
+console.log(SumOfSomething(1,2, square));
